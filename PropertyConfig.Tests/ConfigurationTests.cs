b295fab65356cdd85d93425fad817f7be83bd3ae
bolorundurowb <ogatimo@gmail.com>
2016-12-31 19:19:58 +0100

    test: adds nunit test project

---
+++ PropertyConfig.Tests/ConfigurationTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace PropertyConfig.Tests
+{
+    [TestFixture]
+    public class ConfigurationTests
+    {
+        [Test]
+        public void DummyTest1()
+        {
+            Assert.True(true);
+        }
+    }
+}
