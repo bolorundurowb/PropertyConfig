07b06ac166e4ff52d23e20707c8d778ebf0a30d5
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2018-03-13 12:46:23 +0100

    feature: make the filepath read-only

--- PropertyConfig/Constants.cs
+++ PropertyConfig/Constants.cs
@@ -4,6 +4,6 @@
     {
         // chose to hard-code this because the logic for getting the executing
         // library is too complicated for a small library such as this
-        internal static string LibVersion => "1.1.2";
+        internal static string LibVersion => "2.0.0";
     }
 }
